c8f69592dd51d49da11f3868b6b94bd89bb7b83d
Kevin Herrera <kevin@herrera.io>
2018-02-20 22:46:54 -0800

    Adding the exception class.

---
+++ src/KHerGe/Enum/EnumException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KHerGe\Enum;
+
+use KHerGe\Exception\AbstractException;
+
+/**
+ * An exception thrown for an enum related error.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class EnumException extends AbstractException
+{
+}
